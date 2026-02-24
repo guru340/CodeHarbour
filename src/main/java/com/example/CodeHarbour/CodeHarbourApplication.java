febb31b81051fc9a2ead17b3a4ba6fb34aacfd9f
guru340 <gurusangwani06@gmail.com>
2026-02-24 06:49:50 +0530

    Intial commit

---
+++ src/main/java/com/example/CodeHarbour/CodeHarbourApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeHarbour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHarbourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeHarbourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeHarbour/CodeHarbourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeHarbour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHarbourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
