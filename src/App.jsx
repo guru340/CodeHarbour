5d77b1799877dd713bf1967b32ee7385f23fb470
guru340 <gurusangwani06@gmail.com>
2026-03-04 02:39:59 +0530

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  Home  from './Pages/Home.jsx'
+import { Card } from './components/ui/card'
+import { Button } from './components/ui/button'
+import { MixerHorizontalIcon } from '@radix-ui/react-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
+      <section className='filtersection'>
+        <Card className="p-5 sticky top-10">
+          <div className='flex justify-between lg:w-[20rem]'>
+              <p className='text-xl -tracking-wider'></p>
+                <Button variant="ghost" size="icon">
+                    <MixerHorizontalIcon/>
+                </Button>
+            </div> 
+          
+        </Card>
+      </section>
+
+     </div>
     </>
+  
+   
   )
 }
 
