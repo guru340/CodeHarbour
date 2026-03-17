c48821b1f81da27b324ba4915fb5ac62158f794e
guru340 <gurusangwani06@gmail.com>
2026-03-17 09:01:11 +0530

    Login Navigation Solved

--- src/Redux/Auth/Reducer.js
+++ src/Redux/Auth/Reducer.js
@@ -1,5 +1,5 @@
 
-import { GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType";
+import { GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_REQUEST, LOGIN_SUCCESS,LOGIN_FAILURE, LOGOUT, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType";
 
 
 const intialState={
@@ -22,10 +22,14 @@ const authReducer=(state=intialState,action)=>{
             return{...state,loading:false,error:null,jwt:action.payload.jwt};
 
         case GET_USER_SUCCESS:
+            console.log("reducer GET_USER_SUCCESS:", action.payload)
             return{...state,loading:false,error:null,user:action.payload};
 
         case LOGOUT:
             return intialState;
+        case LOGIN_FAILURE: 
+         return { ...state, loading: false, error: action.payload }
+
         default:
            return state;
     }
