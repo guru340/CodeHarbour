68e94827708ef8b9137f0ced7466a5c833f2aa59
guru340 <gurusangwani06@gmail.com>
2026-03-21 04:02:41 +0530

    Ready TO Deploy

--- src/main/java/com/example/CodeHarbour/Configuration/AppConfig.java
+++ src/main/java/com/example/CodeHarbour/Configuration/AppConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,7 +29,7 @@ public class AppConfig {
                 .authorizeHttpRequests(Authorize->Authorize.requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
                 .addFilterBefore(new JWTValidToken(), BasicAuthenticationFilter.class)
-                .csrf(csrf->csrf.disable())
+                .cors(Customizer.withDefaults()).csrf(csrf->csrf.disable())
                 .cors(cors->cors.configurationSource(corsCofiguration()));
         return http.build();
     }
