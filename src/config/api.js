f2755a70682d7c17925ecade1abf87235eb67446
guru340 <gurusangwani06@gmail.com>
2026-03-21 03:46:07 +0530

    api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_BASE_URL = "http://localhost:8080";    
+export const API_BASE_URL = "https://codeharbour-production.up.railway.app";    
 
 const api=axios.create({baseURL:API_BASE_URL});
 
