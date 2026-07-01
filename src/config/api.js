3ecd7c2619d3d86597a1ae7f7b13f3e0b1ad4ce8
guru340 <gurusangwani06@gmail.com>
2026-07-02 01:36:50 +0530

    BAckend API CHanges

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://codeharbour-production.up.railway.app";
+export const API_BASE_URL = "https://codeharbour-new.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
