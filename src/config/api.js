245bc9b2582d57db1380fc5165352b2332e78e92
guru340 <gurusangwani06@gmail.com>
2026-07-02 14:31:53 +0530

    Backend Link Changed

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://codeharbour-new.onrender.com";
+export const API_BASE_URL = "https://codeharbour-latest4.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
